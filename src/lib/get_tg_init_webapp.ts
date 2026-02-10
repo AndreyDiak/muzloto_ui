1f6b1a7f1b8fc9d5caa1dc3b05de01751453e599
AndreyDiak <brodiaga_0880@mail.ru>
2026-02-11 00:17:05 +0300

    www

--- src/lib/get_tg_init_webapp.ts
+++ src/lib/get_tg_init_webapp.ts
@@ -1,11 +1,9 @@
-import { tgMockData } from "@/_mock/tg_data";
-
 export function getTgWebApp() {
   let webApp = window.Telegram?.WebApp;
   if(webApp?.initData === "") {
     webApp = {
       ...webApp,
-      ...tgMockData
+      // ...tgMockData
     }
   }
 	return webApp
